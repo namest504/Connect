aea43b10cd163c7577e2b148730accb87945dda6
lim1t <namest504@gmail.com>
2023-09-09 04:35:25 +0900

    Feat/eureka setting (#2)
    * feat: Eureka 서버 기본 세팅
    
    - @EnableEurekaServer 어노테이션 적용
    - 환경 변수 설정
    
    * feat: jib를 통한 도커 배포 방식 추가
    
    - 플러그인 추가
    - jib 설정

--- src/main/java/com/lim1t/EurekaServer/EurekaServerApplication.java
+++ src/main/java/com/lim1t/EurekaServer/EurekaServerApplication.java
@@ -2,7 +2,9 @@ package com.lim1t.EurekaServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
 
