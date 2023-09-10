208d053b655ab46eb389e8a01fdcc5b87b317de5
lim1t <namest504@gmail.com>
2023-09-10 23:25:07 +0900

    initial commit

---
+++ src/test/java/com/lim1t/springcloudgateway/SpringcloudgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lim1t.springcloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
