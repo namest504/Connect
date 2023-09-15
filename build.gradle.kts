28d06650ac3846d825adb768007cbea6353b0d50
YJ_Lee <ambition65@naver.com>
2023-09-15 18:38:48 +0900

    build: H2, ModelMapper 디펜던시 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,9 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("com.h2database:h2")
+	implementation("org.modelmapper:modelmapper:3.1.1")
+
 
 	compileOnly("org.projectlombok:lombok")
 
