e90223f501989af288f406fbdf1dc176e2cc241a
YJ_Lee <37949600+ambition98@users.noreply.github.com>
2023-09-20 20:02:22 +0900

    ✨feat: 이미지 업로드 기능 구현 (#8)
    * build: H2, ModelMapper 디펜던시 추가
    
    * feat: H2, ModelMapper 설정 추가
    
    * H2 Bean 생성
    * ModelMapper Bean 생성
        매핑전략: Strict
    
    * 디버깅을 위한 jpa sql 로깅 추가
    
    * feat: 에러 처리 로직 추가 #5
    
    * feat: S3 업로드 기능 추가 #6
    
    - AmazonS3Client 설정 추가
    - S3Util 유틸 클래스 추가
    
    * feat: 이미지 업로드 관련 예외 처리 추가 #6
    
    - 파일 업로드 용량 초과
    - 허용되지 않는 미디어 타입
    - AmazonS3 에러
    
    * refactor: 파일 사이즈 초과 예외 응답변경
    
    - 현재 서버에 설정된 최대 업로드 파일 사이즈를 같이 응답하도록 변경
    
    * feat: 지원하지 았는 Http method 예외처리 추가

--- src/main/java/xyz/connect/post/enumeration/ErrorCode.java
+++ src/main/java/xyz/connect/post/enumeration/ErrorCode.java
@@ -5,12 +5,18 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ErrorCode {
+
     //Common
     SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Z001", "Unknown Server Error"),
-    INVALID_API_PARAMETER(HttpStatus.BAD_REQUEST, "Z002", ""), //ExceptionHandler 에서 메시지 작성
+    THIRD_PARTY_API_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "Z002", ""),
+    NOT_SUPPORTED_METHOD(HttpStatus.BAD_REQUEST, "Z003", "지원하지 않는 Http method 입니다."),
 
     //Post
-    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "P001", "게시글에 대한 권한이 없습니다.");
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "P001", "게시글에 대한 권한이 없습니다."),
+    MAX_UPLOAD_SIZE_EXCEEDED(HttpStatus.BAD_REQUEST, "P002", "파일 업로드 용량이 초과되었습니다."),
+    INVALID_MEDIA_TYPE(HttpStatus.BAD_REQUEST, "P003", "허용되지 않는 이미지 미디어 타입 입니다."),
+    INVALID_API_PARAMETER(HttpStatus.BAD_REQUEST, "Z004", ""), //ExceptionHandler 에서 메시지 작성
+    ;
 
     private final HttpStatus status;
     private final String code;
