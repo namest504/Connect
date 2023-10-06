2d57412153bbeb61c015b41a537ef2336eb93276
suhoon <jobak4200nim@nate.com>
2023-10-06 16:56:42 +0900

    feat: PasswordEncoder 적용 #2

--- src/main/java/xyz/connect/user/web/service/UserService.java
+++ src/main/java/xyz/connect/user/web/service/UserService.java
@@ -2,6 +2,7 @@ package xyz.connect.user.web.service;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import xyz.connect.user.web.entity.UserEntity;
 import xyz.connect.user.web.model.request.CreateUserRequest;
@@ -14,14 +15,17 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
 
     public void createUser(CreateUserRequest createUserRequest) {
-
+        // password 암호화
+        String hashedPassword = bCryptPasswordEncoder.encode(createUserRequest.password());
+        
         //user 객체 생성
         UserEntity userEntity = UserEntity.builder()
                 .email(createUserRequest.email())
-                // TODO: Password Encoder 적용 필요
-                .password(createUserRequest.password())
+                .password(hashedPassword)
                 .build();
         //user save
         userRepository.save(userEntity);
