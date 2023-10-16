65cb6920bfd079229fa156bf575bf8a752895108
YJLee <ambition65@naver.com>
2023-10-16 15:47:59 +0900

    refactor: 이미지 업로드 리턴방식 변경
    - 기존 단순 String 에서 fileName 과 url 을 가진 DTO 를 리턴하도록 변경

--- src/main/java/xyz/connect/post/web/service/UploadImageService.java
+++ src/main/java/xyz/connect/post/web/service/UploadImageService.java
@@ -1,17 +1,17 @@
 package xyz.connect.post.web.service;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import xyz.connect.post.custom_exception.PostApiException;
 import xyz.connect.post.enumeration.ErrorCode;
 import xyz.connect.post.util.S3Util;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import xyz.connect.post.web.model.response.UploadedImage;
 
 @Service
 @RequiredArgsConstructor
@@ -24,23 +24,26 @@ public class UploadImageService {
             "image/webp")
     );
 
-    public String uploadImage(MultipartFile multipartFile) {
+    public UploadedImage uploadImage(MultipartFile multipartFile) {
         if (!isValidImageType(multipartFile)) {
             throw new PostApiException(ErrorCode.INVALID_MEDIA_TYPE);
         }
 
         try {
-            return s3Util.uploadFile(multipartFile);
+            String fileName = s3Util.uploadFile(multipartFile);
+            String url = s3Util.getImageUrl(fileName);
+            return new UploadedImage(fileName, url);
+
         } catch (IOException e) {
             throw new PostApiException(ErrorCode.THIRD_PARTY_API_EXCEPTION);
         }
     }
 
     // 한 개라도 업로드를 허용하지 않는 MediaType 을 가지면 Exception
-    public List<String> uploadImages(List<MultipartFile> multipartFiles) {
-        List<String> fileNames = new ArrayList<>();
-        multipartFiles.forEach(file -> fileNames.add(uploadImage(file)));
-        return fileNames;
+    public List<UploadedImage> uploadImages(List<MultipartFile> multipartFiles) {
+        List<UploadedImage> uploadedImageList = new ArrayList<>();
+        multipartFiles.forEach(file -> uploadedImageList.add(uploadImage(file)));
+        return uploadedImageList;
     }
 
     private boolean isValidImageType(MultipartFile multipartFile) {
