e8bcc0ce2c2b5d20ee2cd8cd89564096c12e07b3
YJLee <ambition65@naver.com>
2023-10-16 18:51:05 +0900

    refactor: DTO 필드 이름 변경 imageUrl -> images

--- src/main/java/xyz/connect/post/web/model/request/UpdatePost.java
+++ src/main/java/xyz/connect/post/web/model/request/UpdatePost.java
@@ -2,7 +2,6 @@ package xyz.connect.post.web.model.request;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
-
 import java.util.List;
 
 
@@ -12,6 +11,6 @@ public record UpdatePost(
         String content,
 
         @Size(max = 10, message = "포스트 내 이미지는 최대 10개까지 가능합니다.")
-        List<String> imageUrl
+        List<String> images
 ) {
 }
