766d62a83188108def150e9216171b40927cc9bf
YJ_Lee <37949600+ambition98@users.noreply.github.com>
2023-10-29 17:10:44 +0900

    구버전 branch에서 작업 후 merge 해서 해당 작업이 날아감  (#28)
    * chore: 개발환경의 Redis 주소 변경
    
    * refactor: PostDetail 삭제
    Comment 는 엔드포인트를 따로 호출하는게 옳다고 판단하여 전체 댓글이 포함되어있는 PostDetail 을 삭제함
    
    * refactor: 기본 PK 인덱싱을 이용하기 위해 default sort 옵션을 createdAt 에서 postId로 변경
    
    * refactor: GET /comment/{postId} 엔드포인트에 페이지네이션 추가

--- src/main/java/xyz/connect/post/web/controller/PostController.java
+++ src/main/java/xyz/connect/post/web/controller/PostController.java
@@ -19,7 +19,6 @@ import xyz.connect.post.util.AccountInfoUtil;
 import xyz.connect.post.web.model.request.CreatePost;
 import xyz.connect.post.web.model.request.UpdatePost;
 import xyz.connect.post.web.model.response.Post;
-import xyz.connect.post.web.model.response.PostDetail;
 import xyz.connect.post.web.service.PostService;
 
 @RestController
@@ -37,7 +36,7 @@ public class PostController {
     }
 
     @GetMapping("/{postId}")
-    public ResponseEntity<PostDetail> getPost(@PathVariable Long postId) {
+    public ResponseEntity<Post> getPost(@PathVariable Long postId) {
         return ResponseEntity.ok(postService.getPost(postId));
     }
 
